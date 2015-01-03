89055294ccc6f8996756687ce90852fae3e4a869
Jon Moss <me@jonathanmoss.me>
2015-01-02 19:06:12 -0500

    Podcast model test

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,3 +1,4 @@
 class Podcast < ActiveRecord::Base
     validates :name, presence: true
+    validates :slug, presence: true
 end
--- spec/models/podcast_spec.rb
+++ spec/models/podcast_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Podcast, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    FactoryGirl.build(:podcast).should be_valid
+  end
+
+  it "is invalid without a name" do
+    FactoryGirl.build(:podcast, :name => nil).should_not be_valid
+  end
+  it "is invalid without a subdomain" do
+    FactoryGirl.build(:podcast, :slug => nil).should_not be_valid
+  end
 end
